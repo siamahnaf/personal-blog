31d86083af5ac1fce51d90b9d53b07a9ee556f2b
Siam Ahnaf <siamahnaf198@gmail.com>
2023-04-29 15:12:40 +0600

    Adding Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
+import { Analytics } from "@vercel/analytics/react"
 
 //Progress
 import NextProgress from "next-progress";
@@ -13,11 +14,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <NextProgress
-        delay={500}
+        delay={1000}
         options={{ showSpinner: false }}
         color="#14b8a6"
       />
       <Component {...pageProps} />
+      <Analytics />
     </ApolloProvider>
   )
 }
